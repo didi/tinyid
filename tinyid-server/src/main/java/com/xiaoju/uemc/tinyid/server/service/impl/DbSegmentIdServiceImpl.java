0de6f1d0a5af3de950696018fe586fd3c046b881
lookingatstarts <lookingatstarts@gmail.com>
2020-05-30 21:30:29 +0800

    修复bug: 事务的隔离级别不对，从DEFAULT(mysql的REPEATABLE_READ)改成READ_COMMITTED

--- tinyid-server/src/main/java/com/xiaoju/uemc/tinyid/server/service/impl/DbSegmentIdServiceImpl.java
+++ tinyid-server/src/main/java/com/xiaoju/uemc/tinyid/server/service/impl/DbSegmentIdServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -28,12 +29,16 @@ public class DbSegmentIdServiceImpl implements SegmentIdService {
 
     /**
      * Transactional标记保证query和update使用的是同一连接
+     * 事务隔离级别应该为READ_COMMITTED,Spring默认是DEFAULT(取决于底层使用的数据库，mysql的默认隔离级别为REPEATABLE_READ)
+     * <p>
+     * 如果是REPEATABLE_READ，那么在本次事务中循环调用tinyIdInfoDAO.queryByBizType(bizType)获取的结果是没有变化的，也就是查询不到别的事务提交的内容
+     * 所以多次调用tinyIdInfoDAO.updateMaxId也就不会成功
      *
      * @param bizType
      * @return
      */
     @Override
-    @Transactional
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public SegmentId getNextSegmentId(String bizType) {
         // 获取nextTinyId的时候，有可能存在version冲突，需要重试
         for (int i = 0; i < Constants.RETRY; i++) {
