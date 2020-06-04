11674eb9b3e66b502d77db137fd92538bd41ef7c
tianzhen <tianzhen@hualala.com>
2020-06-04 15:00:57 +0800

    字段调整

--- tinyid-server/src/main/java/com/xiaoju/uemc/tinyid/server/dao/impl/TinyIdInfoDAOImpl.java
+++ tinyid-server/src/main/java/com/xiaoju/uemc/tinyid/server/dao/impl/TinyIdInfoDAOImpl.java
@@ -22,8 +22,8 @@ public class TinyIdInfoDAOImpl implements TinyIdInfoDAO {
 
     @Override
     public TinyIdInfo queryByBizType(String bizType) {
-        String sql = "select id, biz_type, begin_id, max_id," +
-                " step, delta, remainder, create_time, update_time, version" +
+        String sql = "select id, biz_type, max_id," +
+                " step, delta, remainder, update_time, version" +
                 " from tiny_id_info where biz_type = ?";
         List<TinyIdInfo> list = jdbcTemplate.query(sql, new Object[]{bizType}, new TinyIdInfoRowMapper());
         if(list == null || list.isEmpty()) {
@@ -35,7 +35,7 @@ public class TinyIdInfoDAOImpl implements TinyIdInfoDAO {
     @Override
     public int updateMaxId(Long id, Long newMaxId, Long oldMaxId, Long version, String bizType) {
         String sql = "update tiny_id_info set max_id= ?," +
-                " update_time=now(), version=version+1" +
+                " version = version + 1" +
                 " where id=? and max_id=? and version=? and biz_type=?";
         return jdbcTemplate.update(sql, newMaxId, id, oldMaxId, version, bizType);
     }
@@ -48,12 +48,10 @@ public class TinyIdInfoDAOImpl implements TinyIdInfoDAO {
             TinyIdInfo tinyIdInfo = new TinyIdInfo();
             tinyIdInfo.setId(resultSet.getLong("id"));
             tinyIdInfo.setBizType(resultSet.getString("biz_type"));
-            tinyIdInfo.setBeginId(resultSet.getLong("begin_id"));
             tinyIdInfo.setMaxId(resultSet.getLong("max_id"));
             tinyIdInfo.setStep(resultSet.getInt("step"));
             tinyIdInfo.setDelta(resultSet.getInt("delta"));
             tinyIdInfo.setRemainder(resultSet.getInt("remainder"));
-            tinyIdInfo.setCreateTime(resultSet.getDate("create_time"));
             tinyIdInfo.setUpdateTime(resultSet.getDate("update_time"));
             tinyIdInfo.setVersion(resultSet.getLong("version"));
             return tinyIdInfo;
